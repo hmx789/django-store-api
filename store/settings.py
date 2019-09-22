a9132730c34e6d26e537e90dab15b174fddce10d
hmx789 <murtazah987@gmail.com>
2019-09-22 18:12:18 -0500

    fixing prod url

--- store/settings.py
+++ store/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1', 'www.django-store-api.herokuapp.com']
+ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1', 'django-store-api.herokuapp.com','www.django-store-api.herokuapp.com']
 
 
 # Application definition
